eb5324270d5b36ac32a9cd946ed6d94ac86c04ba
Katie Le <lethikatie@gmail.com>
2020-07-16 21:11:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(fruits)
-  fruits.collect do |fruit,quantity|
-  fruits = {"apple" => 6, "banana" => 10, "watermelon" => 15}
+#def key_for_min_value(fruits)
+ # fruits.collect do |fruit,quantity|
+  #fruits = {"apple" => 6, "banana" => 10, "watermelon" => 15}
+#end
+
+def key_for_min_value(hash)
+  values = hash.values
+  hash.keys[values.index(values.min).to_i]
 end
