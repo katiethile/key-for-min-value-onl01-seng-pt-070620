1e4ca0166f403a892ccbf7212094dc0ad3159a68
Katie Le <lethikatie@gmail.com>
2020-07-16 21:07:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(fruits)
+  fruits.collect do |fruit,quantity|
   fruits = {:apple => 6, :banana => 10, :watermelon => 15}
 key_for_min_value(fruits)
 end
